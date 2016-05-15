72ee7468014e7bf125d6baed5616b55f92418f1b
yakaolife <yakaolife@gmail.com>
2016-05-14 22:48:24 -0700

    Initial Commit

---
+++ HandyCharacterApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HandyCharacterApp
+//
+//  Created by Ya Kao on 5/14/16.
+//  Copyright © 2016 Ya Kao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
