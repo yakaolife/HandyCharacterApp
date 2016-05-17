3cecda7b18d63c79b1637d6bd77e25aca46f1c76
yakaolife <yakaolife@gmail.com>
2016-05-16 21:23:31 -0700

    Set up view controllers

---
+++ HandyCharacterApp/LifeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LifeViewController.swift
+//  HandyCharacterApp
+//
+//  Created by Ya Kao on 5/16/16.
+//  Copyright © 2016 Ya Kao. All rights reserved.
+//
+
+import UIKit
+
+class LifeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
