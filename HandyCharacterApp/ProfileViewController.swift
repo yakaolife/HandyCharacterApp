7258645ddfddb163671e0979b0d1048e5755b82f
yakaolife <yakaolife@gmail.com>
2016-05-17 21:16:34 -0700

    profile view outlets

--- HandyCharacterApp/ProfileViewController.swift
+++ HandyCharacterApp/ProfileViewController.swift
@@ -9,7 +9,38 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-
+    
+    //Top portion:
+    
+    @IBOutlet weak var characterImageView: UIImageView!
+    @IBOutlet weak var characterNameBG: UIImageView!
+    @IBOutlet weak var levelTextField: UITextField!
+    @IBOutlet weak var characterNameLabel: UILabel!
+    
+    //Class & Type
+    
+    @IBOutlet weak var classLabel: UILabel!
+    @IBOutlet weak var raceLabel: UILabel!
+    @IBOutlet weak var backgroundLabel: UILabel!
+    @IBOutlet weak var alignmentLabel: UILabel!
+    
+    //Profile
+    
+    @IBOutlet weak var personalityTraitLabel: UILabel!
+    @IBOutlet weak var backgroundDesLabel: UILabel!
+    @IBOutlet weak var idealLabel: UILabel!
+    @IBOutlet weak var bondLabel: UILabel!
+    @IBOutlet weak var flawLabel: UILabel!
+    
+    //UIViews
+    @IBOutlet weak var classTypeView: UIView!
+    @IBOutlet weak var personalityView: UIView!
+    @IBOutlet weak var backgroundView: UIView!
+    @IBOutlet weak var idealsView: UIView!
+    @IBOutlet weak var bondView: UIView!
+    @IBOutlet weak var flawView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
